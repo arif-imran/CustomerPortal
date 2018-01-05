289cb941ea81a8617f914cb75f371cd1adc3ec17
arif-imran <pk-imran.arif@outlook.com>
2018-01-05 21:22:25 +0500

    Update SharedConfig.cs

--- CustomerPortal.Common/SharedConfig.cs
+++ CustomerPortal.Common/SharedConfig.cs
@@ -2,11 +2,10 @@ namespace CustomerPortal.Common
 {
     public class SharedConfig
     {
-        //public static readonly string AuthServer = "http://customerportalauthdev.grosvenor.com/";
-        //public static readonly string ApiEndpoint = "http://customerportalapidev.grosvenor.com/api/";
+        
 
-        public static readonly string AuthServer = "http://customerportalauthtest.grosvenor.com/";
-        public static readonly string ApiEndpoint = "http://customerportalapitest.grosvenor.com/api/";
+        public static readonly string AuthServer = "http://customerportalauthtest.abctest.com/";
+        public static readonly string ApiEndpoint = "http://customerportalapitest.abctest.com/api/";
         public static readonly string WebUrl = "Website.Url";
         public static readonly string SortDateFormat = "MM/dd/yyyy";
         public static readonly string ShortDateFormat = "dd/MM/yyyy";
